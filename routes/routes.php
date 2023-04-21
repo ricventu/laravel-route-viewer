e2b47329fd9068e575d15fd57b25eb0ddfa0421d
ricventu <ricventu@users.noreply.github.com>
2023-04-21 17:41:32 +0000

    Fix styling

--- routes/routes.php
+++ routes/routes.php
@@ -4,7 +4,6 @@ use Illuminate\Support\Facades\Route;
 use Ricventu\LaravelRouteViewer\Controllers\IndexController;
 use Ricventu\LaravelRouteViewer\Middlewares\HandleInertiaRequests;
 
-
 Route::prefix(config('route-viewer.uri'))
     ->name(config('route-viewer.route_name'))
     ->middleware(HandleInertiaRequests::class)
