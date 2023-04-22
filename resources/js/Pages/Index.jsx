d563a6c9f7936679a6c1b3d0cc31682a86c4893a
Riccardo Venturini <riccardo@fornacestudio.com>
2023-04-22 09:57:19 +0200

    add react-table

--- resources/js/Pages/Index.jsx
+++ resources/js/Pages/Index.jsx
@@ -1,12 +1,75 @@
 import React from 'react'
+import { useTable } from 'react-table/src'
+
+const data = () => [
+  {
+    col1: 'Hello',
+    col2: 'World'
+  }
+]
+
+const columns = () => [
+  {
+    Header: 'Column 1',
+    accessor: 'col1',
+  },
+  {
+    Header: 'Column 2',
+    accessor: 'col2',
+  }
+]
 
 const Index = (props) => {
-  return (
-    <div>
-      <h1>Route Viewer</h1>
-      <p></p>
-    </div>
+  const {
+    getTableProps,
+    getTableBodyProps,
+    headerGroups,
+    rows,
+    prepareRow,
+  } = useTable({ columns, data })
 
+  return (
+    <table {...getTableProps()}>
+      <thead>
+      {// Loop over the header rows
+        headerGroups.map(headerGroup => (
+          // Apply the header row props
+          <tr {...headerGroup.getHeaderGroupProps()}>
+            {// Loop over the headers in each row
+              headerGroup.headers.map(column => (
+                // Apply the header cell props
+                <th {...column.getHeaderProps()}>
+                  {// Render the header
+                    column.render('Header')}
+                </th>
+              ))}
+          </tr>
+        ))}
+      </thead>
+      {/* Apply the table body props */}
+      <tbody {...getTableBodyProps()}>
+      {// Loop over the table rows
+        rows.map(row => {
+          // Prepare the row for display
+          prepareRow(row)
+          return (
+            // Apply the row props
+            <tr {...row.getRowProps()}>
+              {// Loop over the rows cells
+                row.cells.map(cell => {
+                  // Apply the cell props
+                  return (
+                    <td {...cell.getCellProps()}>
+                      {// Render the cell contents
+                        cell.render('Cell')}
+                    </td>
+                  )
+                })}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
   )
 }
 
