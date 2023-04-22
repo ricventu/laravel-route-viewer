afd52505343f22e08086bebaf14ef88884f90585
Riccardo Venturini <riccardo@fornacestudio.com>
2023-04-22 12:08:34 +0200

    wip

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -3,21 +3,23 @@
 namespace Ricventu\LaravelRouteViewer\Controllers;
 
 use Illuminate\Routing\Controller;
-use Illuminate\Routing\Router;
 use Inertia\Inertia;
+use Ricventu\LaravelRouteViewer\LaravelRouteViewer;
 use Ricventu\LaravelRouteViewer\Middlewares\HandleInertiaRequests;
 
 class IndexController extends Controller
 {
-    public function __construct(protected Router $router)
-    {
+    public function __construct(
+        protected LaravelRouteViewer $laravelRouteViewer
+    ) {
         $this->middleware(HandleInertiaRequests::class);
     }
 
     public function __invoke()
     {
+        $routes = $this->laravelRouteViewer->getRoutes();
         return Inertia::render('Index', [
-            'routes' => $this->router->getRoutes()->getRoutes(),
+            'routes' => $routes,
         ]);
     }
 }
--- src/LaravelRouteViewer.php
+++ src/LaravelRouteViewer.php
@@ -2,6 +2,20 @@
 
 namespace Ricventu\LaravelRouteViewer;
 
+use Illuminate\Routing\Router;
+use Illuminate\Support\Str;
+
 class LaravelRouteViewer
 {
+    public function __construct(protected Router $router)
+    {
+    }
+
+    public function getRoutes()
+    {
+        $routes = collect($this->router->getRoutes()->getRoutes());
+        $ignore = config('route-viewer.ignore_routes');
+        $routes = $routes->filter(fn ($route) => !Str::is($ignore, $route->uri));
+        return $routes->values();
+    }
 }
