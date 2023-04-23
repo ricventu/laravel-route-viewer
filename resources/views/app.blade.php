b969b0f6f010d683b6f0c1797d95750527162387
Riccardo Venturini <ricventu@gmail.com>
2023-04-23 12:05:21 +0200

    added tailwindcss

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0"/>
+  {{\Illuminate\Support\Facades\Vite::useBuildDirectory('vendor/route-viewer')->withEntryPoints(['resources/css/app.css']) }}
   {{\Illuminate\Support\Facades\Vite::useBuildDirectory('vendor/route-viewer')->withEntryPoints(['resources/js/app.jsx']) }}
   @inertiaHead
 </head>
